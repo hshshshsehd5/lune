ee33a1d48606ec917d36216fe970c9288c792266
Benstitou Sofiane <129552238+benstitousofiane@users.noreply.github.com>
2023-04-06 13:22:21 +0200

    x^(a/b) function successfuly made with Newton met.

--- lunatique/lunatique.h
+++ lunatique/lunatique.h
@@ -1,10 +1,32 @@
-// power functions
 double powern(double x, int n){
     double result = 1.0;
-    
-    for (int i = 0; i < n ; i++){
+    for (int i = 0; i < n; i++){
         result *= x;
     }
-
     return result;
+}
+
+double powerab(double x, int a, int b){ // Using Newton method : https://www.youtube.com/watch?v=Z_BDJaveZNs&t=337s
+    double result = 4.0;
+    double y = powern(result,b) - x;
+    double yp = b*powern(result,b-1);
+
+    if (a > 0){
+        for (int i = 0; i < 10; i++){
+            result -= y/yp;
+            y = powern(result,b) - x;
+            yp = b*powern(result,b-1);
+        }
+    return powern(result,a);
+    } else {
+        for (int i = 0; i < 10; i++){
+            result -= y/yp;
+            y = powern(result,b) - x;
+            yp = b*powern(result,b-1);
+        }
+
+        return result;
+    }
+
+    
 }
